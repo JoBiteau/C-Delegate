6d39a6186e24c2c0e64483f2bdbf98f8d32dde89
Jo <joffrey.biteau@viacesi.fr>
2020-03-17 09:50:00 +0100

    EX1

---
+++ Calcul.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DelegateApp1
+{
+    public static class Calcul
+    {
+        public static int Additionner(int[] elements)
+        {
+            int resultat = 0;
+
+            foreach(int element in elements)
+            {
+                resultat += element;
+            }
+
+            return resultat;
+        }
+        public static int Multiplier(int[] elements)
+        {
+            int resultat = 1;
+
+            foreach(int element in elements)
+            {
+                resultat = resultat * element;
+            }
+
+            return resultat;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DelegateApp1
+{
+    public delegate int Calculer(int[] elements);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] data = new int[] { 12, 12 };
+
+            Calculer calculer = new Calculer(Calcul.Additionner);
+            calculer += Calcul.Multiplier;
+
+            int result = Compute(data, Calcul.Additionner);
+
+            Console.WriteLine($"{result}");
+            Console.ReadLine();
+        }
+
+        private static int Compute(int[] data, Calculer methodeCalcul)
+        {
+            return methodeCalcul(data);
+        }
+    }
+}
